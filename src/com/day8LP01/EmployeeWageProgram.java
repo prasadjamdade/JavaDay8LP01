79de7e0e5a6c7054b7be859d511fa2a5f49c84cf
Prasad <prasadjamdade@hotmail.com>
2021-11-06 15:03:15 +0530

    Added code to calculate the per day wage for an employee working in Full Time or Part Time schedule.

--- src/com/day8LP01/EmployeeWageProgram.java
+++ src/com/day8LP01/EmployeeWageProgram.java
@@ -4,20 +4,29 @@ A program to simulate Employee wedge.
 To check Employee is Present or Absent.
 */
 
+import java.util.Scanner;
+
 public class EmployeeWageProgram {
     //Main method
     public static void main(String[] args) {
         EmployeeWageProgram methodCaller = new EmployeeWageProgram();
-        methodCaller.attendanceEmployee();
+        methodCaller.wageCalculator();
     }
 
     //Method for finding attendance of Employee.
-    public void attendanceEmployee(){
-        int fullTime = 1;
-        double empCheck = Math.floor(Math.random() * 10) % 2;
-        if (empCheck == fullTime)
-            System.out.println("Employee is Present.");
+    public void wageCalculator() {
+        Scanner shiftTypeInput = new Scanner(System.in);
+        System.out.println("""
+                1. Full Time working schedule.
+                2. Part Time working schedule.
+                """);
+        int shiftTime = shiftTypeInput.nextInt();
+        int empWagePerHour = 20;
+        int workingHours = 8;
+        if (shiftTime == 1) {
+            System.out.println("Employee wage per day for full time schedule is: " + (empWagePerHour * workingHours) + " $");
+        }
         else
-            System.out.println("Employee is Absent.");
+            System.out.println("Employee wage per day for full time schedule is: " + (empWagePerHour * workingHours/2) + " $");
     }
 }
